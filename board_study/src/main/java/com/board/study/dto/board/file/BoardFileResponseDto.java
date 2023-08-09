e084bbadcbcbcf3f1067ee16d239e9a5745e55b8
shyunp13 <shyunp13@gmail.com>
2023-08-09 23:31:07 +0900

    파일업로드 서비스까지 작성 이제 컨트롤러 작성하면됨.

--- board_study/src/main/java/com/board/study/dto/board/file/BoardFileResponseDto.java
+++ board_study/src/main/java/com/board/study/dto/board/file/BoardFileResponseDto.java
@@ -3,8 +3,10 @@ package com.board.study.dto.board.file;
 import com.board.study.entity.board.file.BoardFile;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class BoardFileResponseDto {
 	
 	private String origFileName;
@@ -12,5 +14,11 @@ public class BoardFileResponseDto {
 	private String filePath;
 	
 	public BoardFileResponseDto(BoardFile entity) {
+		this.origFileName = entity.getOrigFileName();
+		this.saveFileName = entity.getSaveFileName();
+		this.filePath = entity.getFilePath();
 	}
+	
+
+	
 }
--- board_study/src/main/java/com/board/study/entity/board/file/BoardFileRepository.java
+++ board_study/src/main/java/com/board/study/entity/board/file/BoardFileRepository.java
@@ -1,6 +1,13 @@
 package com.board.study.entity.board.file;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import jakarta.transaction.Transactional;
 
 public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
 	
@@ -12,5 +19,18 @@ public interface BoardFileRepository extends JpaRepository<BoardFile, Long> {
 	
 	static final String DELETE_BOARD_FILE_YN = "UPDATE board_file"+
 	"SET DELETE_YN='Y'"+
-	"WHERE bOARD_ID IN (:boardIdList)";
+	"WHERE BOARD_ID IN (:boardIdList)";
+	
+	@Query(value = SELECT_FILE_ID, nativeQuery = true)
+	public List<Long> findByBoardId(@Param("boardId")Long boardId);
+	
+	@Transactional
+	@Modifying
+	@Query(value = UPDATE_DELETE_YN, nativeQuery = true)
+	public int updateDeleteYn(@Param("deleteIdList") Long[] deleteIdList);
+	
+	@Transactional
+	@Modifying
+	@Query(value = DELETE_BOARD_FILE_YN, nativeQuery = true)
+	public int deleteBoardFileYn(@Param("boardList") Long[] boardIdList);
 }
